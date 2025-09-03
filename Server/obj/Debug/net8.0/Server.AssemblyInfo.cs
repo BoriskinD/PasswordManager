033703293c977dd00727b7366df6c5f3bc1fb9a8
BoriskinD <shark9797@mail.ru>
2025-09-03 14:01:40 +0300

    fixes

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423119f0868401b9c71a2fb136013855c70dc4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbb4ffc39b19ecea8c4ad8be521fa1136515cee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
