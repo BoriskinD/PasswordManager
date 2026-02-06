0b6035096f1fa439576cd54870ab6c359528c24b
BoriskinD <shark9797@mail.ru>
2026-02-06 13:10:03 +0300

    refactor: code refactor

--- Client/Message.cs
+++ Client/Message.cs
@@ -4,10 +4,10 @@
     public class Message<T> 
     {   
         public T Value { get; set; }
-        public object Sender { get; set; }
+        public object? Sender { get; set; }
         public bool CloseWindow { get; set; }
 
-        public Message(T value, bool closeWindow = false, object sender = null)
+        public Message(T value, bool closeWindow = false, object? sender = null)
         {
             Value = value;
             CloseWindow = closeWindow;
