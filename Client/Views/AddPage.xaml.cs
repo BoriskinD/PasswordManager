0de15e6cf93e86e6cf278d24a04c43bf96e9b805
BoriskinD <shark9797@mail.ru>
2026-02-06 13:47:23 +0300

    refactor: style code refactor

--- Client/Views/AddPage.xaml.cs
+++ Client/Views/AddPage.xaml.cs
@@ -8,7 +8,6 @@ public partial class AddPage : ContentPage
 	public AddPage(AddPageVM addPageVM)
 	{
 		InitializeComponent();
-
 		BindingContext = addPageVM;
 
 		WeakReferenceMessenger.Default.Register<Message<string>,int>(this, (int)MessengerTokens.Tokens.AddPage, (recipient, message) => 
