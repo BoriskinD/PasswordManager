223cb65c5c2aba6ddd27d37187aeaf4ca59792eb
BoriskinD <shark9797@mail.ru>
2026-02-10 15:01:06 +0300

    feat: add Salt field to database

--- Server/Model/User.cs
+++ Server/Model/User.cs
@@ -6,7 +6,9 @@
 
         public string? Login { get; set; }
 
-        public string? PasswordHash { get; set; }
+        public string? Password { get; set; }
+
+        public byte[]? Salt { get; set; } = new byte[0];
 
         public List<Application> Apps { get; set; } = new List<Application>();
     }
