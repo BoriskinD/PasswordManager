1c9bed14e4d8ec5bd26e1ed113d168bad1ee1516
BoriskinD <shark9797@mail.ru>
2026-05-29 12:54:04 +0300

    feat: added ChangeEcnryptedPasswordToBinary migration

---
+++ Server/Data/Migrations/20260421112328_ChangeEncryptedPasswordToBinary.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEncryptedPasswordToBinary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPassword",
+                table: "Application");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "UserPassword",
+                table: "Application",
+                type: "varbinary(64)",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
